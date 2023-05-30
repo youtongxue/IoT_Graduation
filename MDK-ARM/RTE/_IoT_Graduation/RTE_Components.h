8cccccfeecc58b23216aca2f4fdfa106e3d385ea
OneStep <95547598@qq.com>
2023-05-30 17:07:44 +0800

    首次提交

---
+++ MDK-ARM/RTE/_IoT_Graduation/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IoT_Graduation' 
+ * Target:  'IoT_Graduation' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
